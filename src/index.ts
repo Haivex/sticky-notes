a9e075d1f67ad47d14ea80de058f2796dc66b46a
Haivex <demozial@gmail.com>
2021-05-17 16:49:20 +0200

    chore: remove example files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import url from './image.png';
-import product from './product';
-
-product(2, 5);
-const image = new Image();
-image.src = url;
-
 const b = document.createElement('b');
 b.textContent = 'My text';
 
