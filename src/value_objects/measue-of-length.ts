304af3d1933a7308206be0c1c8a8b001e5bd9643
Haivex <demozial@gmail.com>
2021-05-19 13:07:32 +0200

    feat: measure of length

--- src/value_objects/measue-of-length.ts
+++ src/value_objects/measue-of-length.ts
@@ -2,7 +2,14 @@ export default class MeasureOfLength {
   static measureOfLengthRegExp =
     /^(?<measureNumber>\d{1,})(?<unit>[a-z]{1,})$/i;
 
-  private constructor(private measureNumber: number, private unit: string) {}
+  private constructor(
+    public readonly measureNumber: number,
+    public readonly unit: string,
+  ) {}
+
+  get value(): string {
+    return this.measureNumber + this.unit;
+  }
 
   static isValid(measureOfLength: string): boolean {
     return this.measureOfLengthRegExp.test(measureOfLength);
@@ -20,5 +27,15 @@ export default class MeasureOfLength {
       return [Number(measureNumber), unit];
     }
     throw new Error('Wrong format!');
+  }
+
+  static create(measure: number | string): MeasureOfLength {
+    if (typeof measure === 'string') {
+      return new MeasureOfLength(...this.getNumberAndUnit(measure));
+    }
+    if (measure < 0) {
+      throw new Error('Measure can not be negative number');
+    }
+    return new MeasureOfLength(measure, 'px');
   }
 }
