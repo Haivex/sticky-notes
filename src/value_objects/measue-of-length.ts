b946fb9461c803ad4a6a233b31a7ab627d12e60e
Haivex <demozial@gmail.com>
2021-05-19 12:46:23 +0200

    chore: check if valid measure

--- src/value_objects/measue-of-length.ts
+++ src/value_objects/measue-of-length.ts
@@ -1,1 +1,10 @@
-export default class MeasureOfLength {}
+export default class MeasureOfLength {
+  static measureOfLengthRegExp =
+    /^(?<measureNumber>\d{1,})(?<unit>[a-z]{1,})$/i;
+
+  private constructor(private measureNumber: number, private unit: string) {}
+
+  static isValid(measureOfLength: string): boolean {
+    return this.measureOfLengthRegExp.test(measureOfLength);
+  }
+}
