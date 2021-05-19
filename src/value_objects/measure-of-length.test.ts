b1e201091a6d914508acde0ae4a0c49dd9bb15bf
Haivex <demozial@gmail.com>
2021-05-19 13:29:39 +0200

    fix(test): anonymous functions

--- src/value_objects/measure-of-length.test.ts
+++ src/value_objects/measure-of-length.test.ts
@@ -2,24 +2,24 @@ import MeasureOfLength from './measue-of-length';
 
 describe('isValid', () => {
   test('should return true for string: 200px', () => {
-    expect(() => MeasureOfLength.isValid('200px')).toBeTruthy();
+    expect(MeasureOfLength.isValid('200px')).toBeTruthy();
   });
   test('should return true for number: 200', () => {
-    expect(() => MeasureOfLength.isValid('200')).toBeTruthy();
+    expect(MeasureOfLength.isValid(200)).toBeTruthy();
   });
   test('should return false for number: -200', () => {
-    expect(() => MeasureOfLength.isValid(-200)).toBeFalsy();
+    expect(MeasureOfLength.isValid(-200)).toBeFalsy();
   });
   test('should return false for string: -200px', () => {
-    expect(() => MeasureOfLength.isValid('-200px')).toBeFalsy();
+    expect(MeasureOfLength.isValid('-200px')).toBeFalsy();
   });
   test('should return false for string: 200', () => {
-    expect(() => MeasureOfLength.isValid('200')).toBeFalsy();
+    expect(MeasureOfLength.isValid('200')).toBeFalsy();
   });
   test('should return false for string: px200', () => {
-    expect(() => MeasureOfLength.isValid('px200')).toBeFalsy();
+    expect(MeasureOfLength.isValid('px200')).toBeFalsy();
   });
   test('should return false for string: 20px36', () => {
-    expect(() => MeasureOfLength.isValid('20px36')).toBeFalsy();
+    expect(MeasureOfLength.isValid('20px36')).toBeFalsy();
   });
 });
