40d51df39ef3627b55ad78e5565418073a2243ea
Haivex <demozial@gmail.com>
2021-05-21 14:55:03 +0200

    test: getContent and changeContent sticky note

--- src/sticky_note/sticky-note-content.test.ts
+++ src/sticky_note/sticky-note-content.test.ts
@@ -15,4 +15,36 @@ describe('render', () => {
       '',
     );
   });
+
+  test('should render content with given text', () => {
+    content = StickyNoteContent.create('Example text');
+    contentElement = content.render();
+    expect(getByTestId(contentElement, 'paragraph-element').textContent).toBe(
+      'Example text',
+    );
+  });
+});
+
+describe('getContent', () => {
+  test('should return content', () => {
+    const stickyNoteContent = StickyNoteContent.create('Example text');
+    expect(stickyNoteContent.getContent()).toBe('Example text');
+  });
+});
+
+describe('changeContent', () => {
+  test('should change content', () => {
+    const stickyNoteContent = StickyNoteContent.create('Example text');
+    stickyNoteContent.changeContent('Other content');
+    expect(stickyNoteContent.getContent()).toBe('Other content');
+  });
+
+  test('should render with new text', () => {
+    const stickyNoteContent = StickyNoteContent.create('Example text');
+    stickyNoteContent.changeContent('Other content');
+    const contentElement = stickyNoteContent.render();
+    expect(getByTestId(contentElement, 'paragraph-element').textContent).toBe(
+      'Other content',
+    );
+  });
 });
