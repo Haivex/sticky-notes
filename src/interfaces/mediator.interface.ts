fac815afceb2dddf7ed37cd15d269c5deed2b9f5
Haivex <demozial@gmail.com>
2021-05-22 13:52:36 +0200

    fix: create subscriber interface and put into fns

--- src/interfaces/mediator.interface.ts
+++ src/interfaces/mediator.interface.ts
@@ -1,5 +1,9 @@
+export interface Subscriber {
+  (): unknown;
+}
+
 export interface Mediator {
   notify(event: string, data: Record<string, unknown>): void;
-  subscribe(event: string, subscriber: Record<string, unknown>): void;
-  usubscribe(event: string, subscriber: Record<string, unknown>): void;
+  subscribe(event: string, subscriber: Subscriber): void;
+  usubscribe(event: string, subscriber: Subscriber): void;
 }
