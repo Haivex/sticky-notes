197b5e0ff3185b28f002e72b1f75c88a4da3f865
Haivex <demozial@gmail.com>
2021-05-22 13:30:29 +0200

    chore: missing event names in interface

--- src/interfaces/mediator.interface.ts
+++ src/interfaces/mediator.interface.ts
@@ -1,5 +1,5 @@
 export interface Mediator {
   notify(event: string, data: Record<string, unknown>): void;
-  subscribe(subscriber: Record<string, unknown>): void;
-  usubscribe(subscriber: Record<string, unknown>): void;
+  subscribe(event: string, subscriber: Record<string, unknown>): void;
+  usubscribe(event: string, subscriber: Record<string, unknown>): void;
 }
