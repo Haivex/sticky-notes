df315308f9e0f25d86a2594d154c6b5d2f6c88d3
Haivex <demozial@gmail.com>
2021-05-22 13:53:41 +0200

    chore: subscribe method

--- src/sticky_note/sticky-note-mediator.ts
+++ src/sticky_note/sticky-note-mediator.ts
@@ -1,11 +1,17 @@
-import { Mediator } from '../interfaces/mediator.interface';
+import { Mediator, Subscriber } from '../interfaces/mediator.interface';
 
 export default class StickyNoteMediator implements Mediator {
   private events: {
-    [eventName: string]: { (): unknown }[];
+    [eventName: string]: Subscriber[];
   } = {};
 
   notify(event: string, data: Record<string, unknown>): void {
     this.events[event].forEach((fn) => fn.call(data));
+  }
+
+  subscribe(event: string, subscriber: Subscriber): void {
+    if (!this.events[event]) {
+      this.events[event] = [subscriber];
+    }
   }
 }
