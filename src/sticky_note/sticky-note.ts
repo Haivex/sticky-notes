9b20482026cae2fd4a09eb4afffeec7c24cf5cc0
Mateusz Kmieć <64418079+Haivex@users.noreply.github.com>
2021-05-23 16:17:49 +0200

    Sticky note default (#7)
    * chore: merge
    
    * chore: sticky note constructor and render method
    
    * fix: typo
    
    * chore: add size property to sticky note
    
    * fix: target in esbuild
    
    * chore: set sticky note size
    
    * feat: default sticky note facade
    
    * test: sticky note
    
    * chore: test dev
    
    * test: sticky note default
    
    Co-authored-by: Haivex <demozial@gmail.com>

---
+++ src/sticky_note/sticky-note.ts
@@ -0,0 +1,23 @@
+import { Size } from '../interfaces/size.interface';
+import StickyNoteContent from './sticky-note-content';
+import StickyNoteHeader from './sticky-note-header';
+
+export default class StickyNote {
+  private container = document.createElement('div');
+
+  constructor(
+    private stickyNoteHeader: StickyNoteHeader,
+    private stickyNoteContent: StickyNoteContent,
+    private size: Size,
+  ) {}
+
+  render(): HTMLElement {
+    this.container.innerHTML = '';
+    const headerElement = this.stickyNoteHeader.render();
+    const contentElement = this.stickyNoteContent.render();
+    this.container.append(headerElement, contentElement);
+    this.container.style.width = this.size.width.value;
+    this.container.style.height = this.size.height.value;
+    return this.container;
+  }
+}
