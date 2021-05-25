fc670e9602500cb3024d6d8742f766f7be8183f2
Haivex <demozial@gmail.com>
2021-05-25 18:56:48 +0200

    fix: mediator tests

--- src/sticky_note/sticky-note-mediator.test.ts
+++ src/sticky_note/sticky-note-mediator.test.ts
@@ -1,7 +1,17 @@
+import { Subscriber } from '../interfaces/mediator.interface';
 import StickyNoteMediator from './sticky-note-mediator';
 
 const mock1 = jest.fn();
 const mock2 = jest.fn();
+
+const notifyObj1: Subscriber = {
+  callback: mock1,
+  thisRef: {},
+};
+const notifyObj2: Subscriber = {
+  callback: mock2,
+  thisRef: {},
+};
 
 describe('subscribe', () => {
   let mediator: StickyNoteMediator;
@@ -10,34 +20,34 @@ describe('subscribe', () => {
   });
 
   test('should create event if event does not exist', () => {
-    mediator.subscribe('exampleEvent', mock1);
+    mediator.subscribe('exampleEvent', notifyObj1);
     const subscribedFunctions = mediator.getEvents().exampleEvent;
     expect(subscribedFunctions).toBeDefined();
   });
 
   test('should subscribe given function to given event', () => {
-    mediator.subscribe('exampleEvent', mock1);
+    mediator.subscribe('exampleEvent', notifyObj1);
     const subscribedFunctions = mediator.getEvents().exampleEvent;
-    expect(subscribedFunctions).toContain(mock1);
+    expect(subscribedFunctions).toContain(notifyObj1);
   });
 
   test('should not subscribe given function if it is already subscribed to given event', () => {
-    mediator.subscribe('exampleEvent', mock1);
-    mediator.subscribe('exampleEvent', mock1);
+    mediator.subscribe('exampleEvent', notifyObj1);
+    mediator.subscribe('exampleEvent', notifyObj1);
     const subscribedFunctions = mediator.getEvents().exampleEvent;
     expect(subscribedFunctions.length).toBe(1);
   });
 
   test('should have two subscribed functions', () => {
-    mediator.subscribe('exampleEvent', mock1);
-    mediator.subscribe('exampleEvent', mock2);
+    mediator.subscribe('exampleEvent', notifyObj1);
+    mediator.subscribe('exampleEvent', notifyObj2);
     const subscribedFunctions = mediator.getEvents().exampleEvent;
     expect(subscribedFunctions.length).toBe(2);
   });
 
   test('should subscribe the same function to two different events', () => {
-    mediator.subscribe('exampleEvent', mock1);
-    mediator.subscribe('exampleEvent2', mock1);
+    mediator.subscribe('exampleEvent', notifyObj1);
+    mediator.subscribe('exampleEvent2', notifyObj1);
     const subscribedFunctionsToEventOne = mediator.getEvents().exampleEvent;
     const subscribedFunctionsToEventTwo = mediator.getEvents().exampleEvent2;
     expect(subscribedFunctionsToEventOne.length).toBe(1);
@@ -49,35 +59,35 @@ describe('unsubscribe', () => {
   let mediator: StickyNoteMediator;
   beforeEach(() => {
     mediator = new StickyNoteMediator();
-    mediator.subscribe('exampleEvent', mock1);
-    mediator.subscribe('exampleEvent', mock2);
-    mediator.subscribe('exampleEvent2', mock1);
-    mediator.subscribe('exampleEvent2', mock2);
+    mediator.subscribe('exampleEvent', notifyObj1);
+    mediator.subscribe('exampleEvent', notifyObj2);
+    mediator.subscribe('exampleEvent2', notifyObj1);
+    mediator.subscribe('exampleEvent2', notifyObj2);
   });
 
   test('should unsubscribe given function from given event', () => {
-    mediator.unsubscribe('exampleEvent', mock1);
+    mediator.unsubscribe('exampleEvent', notifyObj1);
     const subscribedFunctions = mediator.getEvents().exampleEvent;
     expect(subscribedFunctions.length).toBe(1);
-    expect(subscribedFunctions).not.toContain(mock1);
-    expect(subscribedFunctions).toContain(mock2);
+    expect(subscribedFunctions).not.toContain(notifyObj1);
+    expect(subscribedFunctions).toContain(notifyObj2);
   });
 
   test('should do nothing if function is not subscribed', () => {
-    mediator.unsubscribe('exampleEvent', mock1);
-    mediator.unsubscribe('exampleEvent', mock1);
+    mediator.unsubscribe('exampleEvent', notifyObj1);
+    mediator.unsubscribe('exampleEvent', notifyObj1);
     const subscribedFunctions = mediator.getEvents().exampleEvent;
     expect(subscribedFunctions.length).toBe(1);
-    expect(subscribedFunctions).not.toContain(mock1);
-    expect(subscribedFunctions).toContain(mock2);
+    expect(subscribedFunctions).not.toContain(notifyObj1);
+    expect(subscribedFunctions).toContain(notifyObj2);
   });
 
   test('should unsubscribe given function only from given event', () => {
-    mediator.unsubscribe('exampleEvent', mock1);
+    mediator.unsubscribe('exampleEvent', notifyObj1);
     const subscribedFunctionsFromEventOne = mediator.getEvents().exampleEvent;
     const subscribedFunctionsFromEventTwo = mediator.getEvents().exampleEvent2;
-    expect(subscribedFunctionsFromEventOne).not.toContain(mock1);
-    expect(subscribedFunctionsFromEventTwo).toContain(mock1);
+    expect(subscribedFunctionsFromEventOne).not.toContain(notifyObj1);
+    expect(subscribedFunctionsFromEventTwo).toContain(notifyObj1);
   });
 });
 
@@ -85,18 +95,18 @@ describe('notify', () => {
   let mediator: StickyNoteMediator;
   beforeEach(() => {
     mediator = new StickyNoteMediator();
-    mediator.subscribe('exampleEvent', mock1);
-    mediator.subscribe('exampleEvent', mock2);
+    mediator.subscribe('exampleEvent', notifyObj1);
+    mediator.subscribe('exampleEvent', notifyObj2);
   });
   test('should call subscribed functions', () => {
-    mediator.notify('exampleEvent', {});
+    mediator.notify('exampleEvent', { value: undefined });
     expect(mock1).toBeCalledTimes(1);
     expect(mock2).toBeCalledTimes(1);
   });
   test('should call subscribed functions with given data', () => {
-    const testedData = { test: 'test value' };
+    const testedData = { value: 'test value' };
     mediator.notify('exampleEvent', testedData);
-    expect(mock1).toBeCalledWith(testedData);
-    expect(mock2).toBeCalledWith(testedData);
+    expect(mock1).toBeCalledWith(testedData.value);
+    expect(mock2).toBeCalledWith(testedData.value);
   });
 });
