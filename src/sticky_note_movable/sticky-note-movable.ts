cb4c5955a3be15b4334a9b7675c9042f2e45a32d
Haivex <demozial@gmail.com>
2021-05-31 21:38:17 +0200

    chore: drag without drag event

--- src/sticky_note_movable/sticky-note-movable.ts
+++ src/sticky_note_movable/sticky-note-movable.ts
@@ -5,24 +5,32 @@ export default class MovableStickyNote extends StickyNote {
 
   private offsetY = 0;
 
+  private isMoving = false;
+
   render(): HTMLElement {
     super.render();
-    this.container.draggable = true;
 
-    this.container.addEventListener('mousemove', (e) => {});
-
-    this.container.addEventListener('dragstart', (e) => {
+    this.container.addEventListener('mousedown', (e) => {
+      e.preventDefault();
+      this.isMoving = true;
       this.offsetX = e.offsetX;
       this.offsetY = e.offsetY;
     });
 
-    this.container.addEventListener('drag', (e) => {
-      this.container.style.left = `${e.x - this.offsetX}px`;
-      this.container.style.top = `${e.y - this.offsetY}px`;
-      // this.container.style.top = `${e.pageY}px`;
+    this.container.addEventListener('mousemove', (e) => {
+      e.preventDefault();
+      if (this.isMoving) {
+        if (e.x - this.offsetX > 0) {
+          this.container.style.left = `${e.x - this.offsetX}px`;
+        }
+        if (e.y - this.offsetY > 0)
+          this.container.style.top = `${e.y - this.offsetY}px`;
+      }
     });
 
-    this.container.addEventListener('dragend', (e) => {
+    this.container.addEventListener('mouseup', (e) => {
+      e.preventDefault();
+      this.isMoving = false;
       this.container.style.left = `${e.x - this.offsetX}px`;
       this.container.style.top = `${e.y - this.offsetY}px`;
     });
