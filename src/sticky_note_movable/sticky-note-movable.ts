f2488d307bd6baf24653279b1c49cef7d1aab713
Haivex <demozial@gmail.com>
2021-06-11 16:52:34 +0200

    fix: moving inside container X

--- src/sticky_note_movable/sticky-note-movable.ts
+++ src/sticky_note_movable/sticky-note-movable.ts
@@ -3,6 +3,10 @@ import { StickyNote } from '../interfaces/sticky-note/sticky-note.interface';
 const makeMovable = (note: StickyNote): StickyNote => {
   const givenContainer = note.container;
 
+  const notesContainer = document.querySelector(
+    '.notesContainer',
+  ) as HTMLElement;
+
   note.mediator.subscribe('moveTriggered', {
     callback: () => {
       if (note.state === 'moving') {
@@ -16,19 +20,28 @@ const makeMovable = (note: StickyNote): StickyNote => {
     thisRef: note,
   });
 
-  let offsetX = 0;
+  let startX = 0;
 
-  let offsetY = 0;
+  let startY = 0;
 
   let isMoving = false;
+
+  const notesContainerPosX = notesContainer.getBoundingClientRect().left;
+  const notesContainerPosY = notesContainer.getBoundingClientRect().top;
+
+  const notesContainerPosEndX =
+    notesContainer.getBoundingClientRect().left +
+    notesContainer.getBoundingClientRect().width;
+  const notesContainerPosEndY =
+    notesContainer.getBoundingClientRect().top +
+    notesContainer.getBoundingClientRect().height;
 
   givenContainer.addEventListener('mousedown', (e) => {
     if (note.state === 'moving') {
       e.preventDefault();
-      givenContainer.style.position = 'absolute';
       isMoving = true;
-      offsetX = e.offsetX;
-      offsetY = e.offsetY;
+      startX = givenContainer.getBoundingClientRect().left - e.pageX;
+      startY = givenContainer.getBoundingClientRect().top - e.pageY;
     }
   });
 
@@ -36,10 +49,35 @@ const makeMovable = (note: StickyNote): StickyNote => {
     if (note.state === 'moving') {
       e.preventDefault();
       if (isMoving) {
-        if (e.x - offsetX > 0) {
-          givenContainer.style.left = `${e.x - offsetX}px`;
+        let currentPositionX = e.x + startX;
+        let currentPositionY = e.y + startY;
+
+        if (currentPositionX < notesContainerPosX + 8) {
+          currentPositionX = notesContainerPosX + 8;
         }
-        if (e.y - offsetY > 0) givenContainer.style.top = `${e.y - offsetY}px`;
+
+        if (
+          currentPositionX + givenContainer.clientWidth >
+          notesContainerPosEndX - 8
+        ) {
+          currentPositionX =
+            notesContainerPosEndX - 8 - givenContainer.clientWidth;
+        }
+
+        if (currentPositionY < notesContainerPosY + 8) {
+          currentPositionY = notesContainerPosY + 8;
+        }
+
+        if (
+          currentPositionY + givenContainer.clientHeight >
+          notesContainerPosEndY - 8
+        ) {
+          currentPositionX =
+            notesContainerPosEndX - 8 - givenContainer.clientHeight;
+        }
+
+        givenContainer.style.left = `${currentPositionX}px`;
+        givenContainer.style.top = `${currentPositionY}px`;
       }
     }
   });
